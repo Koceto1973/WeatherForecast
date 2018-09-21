4325573becc7cb40eeabdcc1f3e03cd167b4d187
K.K <koceto1973@yahoo.ca>
2018-09-21 13:55:01 +0300

    ib outlets and actions

--- WeatherForecast/ViewController.swift
+++ WeatherForecast/ViewController.swift
@@ -7,20 +7,54 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var cityTextField: UITextField!
     @IBOutlet weak var button: UIButton!
+    @IBOutlet weak var label: UILabel!
+    
+    var www:String = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        cityTextField.delegate = self
+        
         button.layer.cornerRadius = 10
         button.layer.borderColor = UIColor.white.cgColor
         button.layer.borderWidth = 2.0
+        
     }
 
-
+    @IBAction func SubmitCity(_ sender: UIButton) {
+        if  cityTextField.text != ""  {
+            www = "https://www.weather-forecast.com/locations/"
+            www += cityTextField.text ?? "London"
+            www += "/forecasts/latest"
+            label.text = www
+            
+            // continue
+            
+            
+        } else {
+            label.text = "Provide your city name"
+        }
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField:UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
 }
+
+    
+
 
